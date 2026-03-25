9b20b8e2728c54466244f4ca338bb347cf6a44fb
kristian-gjyshja <kristian.gjyshja.studenti@isii.it>
2026-03-25 19:48:23 +0100

    cambio colore login

--- public/templates/head.php
+++ public/templates/head.php
@@ -8,4 +8,4 @@
   <link rel="stylesheet" href="<?= htmlspecialchars(asset('css/admin-ui.css')) ?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
-<body class="bg-slate-100">
+<body class="bg-white">
